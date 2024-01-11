f07b953b0b3809a074d94a99ac4d44989305fbcd
LDepaul <129820888+LDepaul@users.noreply.github.com>
2024-01-10 23:54:08 -0300

    Add files via upload

---
+++ proyecto-js-coder/clase-1/main.js
@@ -0,0 +1,50 @@
+// let edad = 50;
+// let nombre = "luca";
+// console.log("¿cual es el nombre?", nombre);
+
+// let nombre = prompt("ingrese su nombre");
+// alert("hola" + " " + nombre);
+
+// let number = Number(prompt("ingrese su fecha de nacimiento"));
+// alert(2024 - number + " " + "esta es tu edad")
+
+// let edad = Number(prompt("ingrese su edad"))
+// if(edad < 14){
+//     alert("No puedes entrar a la fiesta")
+
+// }else if (edad < 18){
+//     alert("Puedes entrar acompañado por un adulto responsable")
+
+// }else{
+//     alert("Puede pasar")
+// }
+
+// let numero = Number(prompt("ingrese un numero"))
+// if(numero > 1000){
+//     alert("el numero ingresado es mayor a 1000")
+// }
+
+// let texto = Number(prompt("ingrese un texto"))
+// if(texto == hola){
+//     console.log("alerta")
+// }
+
+// let texto = prompt('Ingrese un un texto');
+
+// while (texto !== 'ESC') {
+
+//     for (let i= 1; i < 5;i++){
+
+//         texto=prompt('Ingrese un un texto');
+
+//         alert(`Texto ${i} ${texto} `);
+
+//     }
+
+// }
+
+
+
+
+
+
